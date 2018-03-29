2df5f69efa82511c04cfe33c4921024bfaffa88a
Muhammad Yoga Nugraha Putra <37873825+Putra6706162008@users.noreply.github.com>
2018-03-29 08:56:39 +0700

    Update Fibonacci.java

--- putra/src/Fibonacci.java
+++ putra/src/Fibonacci.java
@@ -3,20 +3,25 @@ import java.util.Scanner;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        Scanner show = new Scanner(System.in);
-        System.out.print("Masukan Jumlah Deret Fibonacci: ");
-        int n = show.nextInt();
-        long fib[] = new long[n];
+        System.out.println("Masukan Jumlah Deret Fibonacci: ");
+        int number = new Scanner(System.in).nextInt();
+        fibonacciLoop(number);
+    }
 
-        fib[0] = 0;
-        fib[1] = 1;
-
-        for(int i = 2; i < n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
+    public static int fibonacciLoop(int number) {
+        if (number == 1 || number == 2) {
+            return 1;
         }
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
+        int fibo1 = 1, fibo2 = 1, fibonacci = 1;
+        int sum = 2;
+        for (int i = 3; i <= number; i++) {
+            fibonacci = fibo1 + fibo2;
+            fibo1 = fibo2;
+            fibo2 = fibonacci;
+            sum = sum + fibonacci;
         }
+        System.out.println("Jumlah total input deret fibonacci: " +sum);
+        return fibonacci; // Fibonacci number
     }
 }
